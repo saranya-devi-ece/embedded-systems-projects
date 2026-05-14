3b86fefd9010dd8d6164271b2483895d8f9fb039
saranya-devi <saranyadevi1652006@gmail.com>
2026-05-14 11:42:04 +0530

    Enhance comments for switch logic in LED control
    Added comments to clarify switch logic for LED control.

--- Button_Interfacing/Push_Button_LED.ino
+++ Button_Interfacing/Push_Button_LED.ino
@@ -4,19 +4,21 @@ int led1 = 3;
 int led2 = 5;
 
 void setup(){
-  pinMode(sw1, INPUT_PULLUP);
-  pinMode(sw2, INPUT);
+  pinMode(sw1, INPUT_PULLUP); // Negative Logic Switch
+  pinMode(sw2, INPUT);  // Positive Logic Switch
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
 }
 
 void loop(){
+ // Negative Logic - press=LOW → Positve Logic LED ON
   if(digitalRead(sw1) == LOW){
     digitalWrite(led1, HIGH);
   }else{
     digitalWrite(led1,LOW);
   }
-   
+
+  // Positive Logic - press=LOW → Negative Logic LED ON
   if(digitalRead(sw2) == HIGH){
     digitalWrite(led2, LOW);
   }else{
