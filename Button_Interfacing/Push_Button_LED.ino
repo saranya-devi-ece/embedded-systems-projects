0876e875d7b70c2679713ad9c85b979ac5d90d8f
saranya-devi <saranyadevi1652006@gmail.com>
2026-05-14 11:34:05 +0530

    Add button and LED control functionality

--- Button_Interfacing/Push_Button_LED.ino
+++ Button_Interfacing/Push_Button_LED.ino
@@ -1,1 +1,25 @@
+int sw1 = 2;
+int sw2 = 4;
+int led1 = 3;
+int led2 = 5;
 
+void setup(){
+  pinMode(sw1, INPUT_PULLUP);
+  pinMode(sw2, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+void loop(){
+  if(digitalRead(sw1) == LOW){
+    digitalWrite(led1, HIGH);
+  }else{
+    digitalWrite(led1,LOW);
+  }
+   
+  if(digitalRead(sw2) == HIGH){
+    digitalWrite(led2, LOW);
+  }else{
+    digitalWrite(led2, HIGH);
+  }
+}
